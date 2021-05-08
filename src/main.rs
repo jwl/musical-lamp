2079414a2f4a7b0d713968c5dc3103c8ab9850f8
jwl <james.warren.leung@pm.me>
2021-05-08 13:18:47 -0700

    Updated to end of 4.3.3

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ use musical_lamp::configuration::get_configuration;
 use musical_lamp::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read config
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    // init logger
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
