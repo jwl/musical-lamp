91e9fcd947403a553c348307f578e1386903bcaa
jwl <james.warren.leung@pm.me>
2021-06-12 13:02:22 -0700

    Updated to end of 5.3.7

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,10 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     // Setup DB
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    //let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
